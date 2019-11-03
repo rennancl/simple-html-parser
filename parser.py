95ec7b4bba0a276ee174ef88d5236e0f274cc721
Rennan Cordeiro <rennancl@gmail.com>
2019-11-03 10:13:34 -0300

    parser improved

--- parser.py
+++ parser.py
@@ -1,22 +1,45 @@
+import json
 from bs4 import BeautifulSoup
 
+def norm_string(text):
+    return text.lower().replace("\t", "").rstrip().replace(":", "")
 
-with open('/home/rennancordeiro/Documents/latin/78/39035.html', 'r') as f:
-    htmltxt = f.read().replace('\n', '')
+def print_json(text):
+    text = json.dumps(text, indent=4, sort_keys=True, ensure_ascii=False)
+    print(text)
 
 
-soup = BeautifulSoup(htmltxt, 'lxml')
-att = soup.findAll("div", {"class": "control-group"})
-# print(dir(att[19]))
+def open_and_load(file):
+    with open(file, 'r') as f:
+        htmltxt = f.read().replace('\n', '')
+    soup = BeautifulSoup(htmltxt, 'lxml')
+    return soup
 
-page = {}
-for at in att:
-    try:
-        page[at.contents[0].contents[0]] = at.contents[2].contents[0]
-        # print(at.contents[0].contents[0])
-        # print(at.contents[2].contents[0])
-    except:
-        pass
-        continue
+def parse(soup):
+    att = soup.findAll("div", {"class": "control-group"})
+    parsed = {}
+    for at in att:
+        try:
+            parsed[norm_string(at.contents[0].contents[0])] = norm_string(at.contents[2].contents[0])
+        except:
+            continue
 
+    parsed["sub área"] = parsed["área predominante"].split(";")[1][1:]
+    parsed["área predominante"] = parsed["área predominante"].split(";")[0]
+
+    att = soup.findAll("h1", {"style": "position: relative;"})[0].contents[0]
+    parsed["name"] = norm_string(str(att)[1:])
+    return parsed
+
+for i in range(1, 500):
+    print(i)
+    group = './0/{}.html'.format(i)
+    soup = open_and_load(group)
+    parsed = parse(soup)
+    print_json(parsed)
+    break
+
+# soup = open_and_load('./0/400.html')
+# parsed = parse(soup)
+# print_json(parsed)
 
